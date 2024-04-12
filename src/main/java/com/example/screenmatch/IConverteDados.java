890e5a1922617c39fa7100226812ef8faf495660
Fernando Lusvarghi <fernando.lusvarghi@serpro.gov.br>
2024-04-12 11:41:47 -0300

    first commit

---
+++ src/main/java/com/example/screenmatch/IConverteDados.java
@@ -0,0 +1,9 @@
+package com.example.screenmatch;
+
+
+
+public interface IConverteDados {
+	  <T> T  obterDados(String json, Class<T> classe);
+   
+}
+
---
+++ src/main/java/com/example/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,31 @@
+package com.example.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+		
+	}
+	
+	 @Override
+	    public void run(String... args) throws Exception {
+		    ConsumoApi consumoApi = new ConsumoApi();
+		    String json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=1f56cb87");
+
+		    
+		    
+
+//		    json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+//		    System.out.println(json);
+		    System.out.println(json);
+		    ConverteDados conversor = new ConverteDados();
+		    DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		    System.out.println(dados);
+	 }
+
+}
